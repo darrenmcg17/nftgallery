dd5bb8006ce55b243b81b56d6a90fb1a32726296
darrenmcg17 <72937348+darrenmcg17@users.noreply.github.com>
2022-10-01 14:28:54 +0100

    Added fetch by collection function

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,6 +4,7 @@ const Home = () => {
 const [wallet, setWalletAddress] = useState("");    
 const [collection, setCollectionAddress] = useState("");
 const [NFTs, setNFTs] = useState([]);
+const [fetchForCollection, setFetchForCollection]=useState(false);
  
 const fetchNFTs = async() => {
     let nfts;
@@ -52,7 +53,7 @@ const fetchNFTsForCollection = async () => {
             <div className="flex flex-col w-full justify-center items-center gap-y-2">
                 <input type={"text"} placeholder="Add wallet address"></input>
                 <input type={"text"} placeholder="Add collection address"></input>
-                <label className="text-gray-600"><input type={"checkbox"} className="mr-2"></input>Fetch for collection.</label>
+                <label className="text-gray-600"><input onChange={(e)=>{setFetchForCollection(e.target.checked)}} type={"checkbox"} className="mr-2"></input>Fetch for collection.</label>
                 <button className={"disabled:bg-slate-500 text-white bg-green-400 px-4 py-2 mt-3 rounded-sm w-1/5"} onClick={
                   () => {
 
