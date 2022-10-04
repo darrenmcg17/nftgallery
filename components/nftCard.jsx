e2809b3c4f7df3eb7cd6a50f68ee7a88bc81c972
darrenmcg17 <72937348+darrenmcg17@users.noreply.github.com>
2022-10-04 23:32:30 +0100

    Changed code for clipboard button

--- components/nftCard.jsx
+++ components/nftCard.jsx
@@ -11,12 +11,11 @@ export const NFTCard = ({ nft }) => {
                 <div className="">
                     <h2 className="text-xl text-gray-600">{nft.title}</h2>
                     <p className="text-gray-300 overflow-hidden">{nft.id.tokenId}</p>
-                    <a onClick={() => {navigator.clipboard.writeText("http://localhost:3000/"); alert("Copied to clipboard.");}}
-                    class="bg-slate-400 hover:bg-white text-gray-600 py-2 px-4 rounded inline-flex items-center overflow-hidden">
+                    <p className="bg-slate-400 text-gray-600 py-2 px-4 rounded items-center overflow-hidden">
                         
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <svg onClick={() => {navigator.clipboard.writeText(nft.contract.address); alert("Copied to clipboard.");}} xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />
-                        </svg>{nft.contract.address}</a>
+                        </svg>{nft.contract.address}</p>
                 </div>
 
             </div>
